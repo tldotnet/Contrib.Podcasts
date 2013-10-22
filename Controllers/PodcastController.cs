17ceb08214a8ea5e7b5c03e4e47198b190220d80
AndrewConnell <me@andrewconnell.com>
2013-10-22 17:53:16 -0400

    Resolved issues #15 and #18

---
+++ Controllers/PodcastController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Contrib.Podcasts.Models;
+using Contrib.Podcasts.Services;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Core.Feeds;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Shapes;
+using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.Mvc;
+using Orchard.Settings;
+using Orchard.Themes;
+using Orchard.UI.Navigation;
+
+namespace Contrib.Podcasts.Controllers {
+  [Themed]
+  public class PodcastController : Controller {
+    private readonly IOrchardServices _services;
+    private readonly IPodcastService _podcastService;
+    private readonly IPodcastEpisodeService _podcastEpisodeService;
+    private readonly ISiteService _siteService;
+
+    public PodcastController(IOrchardServices services, IPodcastService podcastService, IPodcastEpisodeService podcastEpisodeService, IShapeFactory shapeFactory, ISiteService siteService) {
+      _services = services;
+      _podcastService = podcastService;
+      _podcastEpisodeService = podcastEpisodeService;
+      _siteService = siteService;
+      Logger = NullLogger.Instance;
+      Shape = shapeFactory;
+      T = NullLocalizer.Instance;
+    }
+
+    dynamic Shape { get; set; }
+    protected ILogger Logger { get; set; }
+    public Localizer T { get; set; }
+
+    public ActionResult Item(int podcastId, PagerParameters pagerParameters) {
+      Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+
+      var podcastPart = _podcastService.Get(podcastId).As<PodcastPart>();
+      if (podcastPart == null)
+        return HttpNotFound();
+
+      if (!_services.Authorizer.Authorize(Orchard.Core.Contents.Permissions.ViewContent, podcastPart, T("Cannot view content"))) {
+        return new HttpUnauthorizedResult();
+      }
+
+
+      var podcastEpisodes = _podcastEpisodeService.Get(podcastPart, pager.GetStartIndex(), pager.PageSize)
+          .Select(b => _services.ContentManager.BuildDisplay(b, "Summary"));
+      dynamic podcast = _services.ContentManager.BuildDisplay(podcastPart);
+
+      var list = Shape.List();
+      list.AddRange(podcastEpisodes);
+      podcast.Content.Add(Shape.Parts_Podcasts_PodcastEpisode_List(ContentItems: list), "5");
+
+      var totalItemCount = _podcastEpisodeService.EpisodeCount(podcastPart);
+      podcast.Content.Add(Shape.Pager(pager).TotalItemCount(totalItemCount), "Content:after");
+
+      return new ShapeResult(this, podcast);
+    }
+
+  }
+}
--- Handlers/PodcastHandler.cs
+++ Handlers/PodcastHandler.cs
@@ -1,13 +1,35 @@
-﻿using Contrib.Podcasts.Models;
+﻿using System.Web.Routing;
+using System.Web.UI.WebControls;
+using Contrib.Podcasts.Models;
+using Contrib.Podcasts.Routing;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Aspects;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Data;
 
 namespace Contrib.Podcasts.Handlers {
   public class PodcastHandler : ContentHandler {
+    private readonly IPodcastPathConstraint _podcastPathConstraint;
 
-    public PodcastHandler(IRepository<PodcastPartRecord> podcastPartRepository) {
+    public PodcastHandler(IRepository<PodcastPartRecord> podcastPartRepository, IPodcastPathConstraint podcastPathConstraint) {
+      _podcastPathConstraint = podcastPathConstraint;
+
       Filters.Add(StorageFilter.For(podcastPartRepository));
+
+      OnCreated<PodcastPart>((context, podcast) => _podcastPathConstraint.AddPath(podcast.As<IAliasAspect>().Path));
+      OnRemoved<PodcastPart>((context, podcast) => _podcastPathConstraint.RemovePath(podcast.As<IAliasAspect>().Path));
     }
 
+    protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+      var podcast = context.ContentItem.As<PodcastPart>();
+      if (podcast == null) return;
+
+      context.Metadata.DisplayRouteValues = new RouteValueDictionary {
+                {"Area", "Contrib.Podcasts"},
+                {"Controller", "Podcast"},
+                {"Action", "Item"},
+                {"podcastId", context.ContentItem.Id}
+            };
+    }
   }
 }
