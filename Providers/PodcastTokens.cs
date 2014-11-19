fe39dc8195868089c043c11e9939e18be7619764
Dotnet <tl.dotnet@deftoft.com>
2014-11-19 16:48:41 +0530

    Episode Podcast Url
    Podcast Episode URL contains Three Digits Episode Numbers

--- Providers/PodcastTokens.cs
+++ Providers/PodcastTokens.cs
@@ -2,9 +2,12 @@
 using Orchard.ContentManagement;
 using Orchard.Localization;
 using Orchard.Tokens;
-
-namespace Contrib.Podcasts.Providers {
+using System;
+namespace Contrib.Podcasts.Providers
+{
+    
   public class PodcastTokens : ITokenProvider {
+      private readonly IContentManager _contentManager;
     public PodcastTokens() {
       T = NullLocalizer.Instance;
     }
@@ -15,7 +18,11 @@ namespace Contrib.Podcasts.Providers {
       context.For("Content", T("Content Items"), T("Content Items"))
         .Token("PodcastEpisode", T("PodcastEpisode"), T("Provides access to the PodcastEpisode part."), "PodcastEpisode");
       context.For("PodcastEpisode", T("Podcast Episode Items"), T("Tokens for podcast text strings"))
-        .Token("EpisodeNumber", T("Episode Number"), T("Includes the episode number."));
+        .Token("EpisodeNumber", T("Episode Number"), T("Includes the episode number."))
+          .Token("FormatEpisodeNumber", T("Episodenumber"), T("Format Specifier For EpisodeNumber"));
+       ;
+       
+
     }
 
     public void Evaluate(EvaluateContext context) {
@@ -23,7 +30,12 @@ namespace Contrib.Podcasts.Providers {
         .Token("PodcastEpisode", content => content.As<PodcastEpisodePart>())
         .Chain("PodcastEpisode", "PodcastEpisode", content => content.As<PodcastEpisodePart>());
       context.For<PodcastEpisodePart>("PodcastEpisode")
-        .Token("EpisodeNumber", content => content.EpisodeNumber);
+          .Token("EpisodeNumber",content => content.EpisodeNumber)
+           .Token("FormatEpisodeNumber",content =>  string.Format("{0:000}",content.EpisodeNumber))
+                   
+          ;
     }
+
+    
   }
 }
